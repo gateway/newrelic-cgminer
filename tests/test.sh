60072046996b64eb8eb457b3d2550068187cbd27
Siim Raud <siim@qminderapp.com>
2013-12-29 23:08:50 +0200

    Improved test script.

--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+assert_return_code()
+{
+	if [ $1 != $2 ]
+	then
+		echo Expected return code $2. Actual code is $1.
+		exit -1
+	fi
+}
+
 python main.py
+assert_return_code $? 2
+
+exit 0
