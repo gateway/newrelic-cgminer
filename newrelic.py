2d737211c85d7c8ac9a6ed195724cd1bd77797db
Siim Raud <siim@qminderapp.com>
2013-12-30 18:05:53 +0200

    Retrying to send data on failure.

--- newrelic.py
+++ newrelic.py
@@ -42,6 +42,15 @@ class Agent:
 			LOGGER.debug('Response: %s: %r',
 						response.status_code,
 						response.content.strip())
+						
+			if response.status_code == 403:
+				LOGGER.error('New Relic request forbidden because of a bad licence key. Exiting.')
+				sys.exit()
+			if response.status_code >= 500:
+				LOGGER.info('New relic did not accept the data. Trying again in 5 seconds')
+				time.sleep(5)
+				self.send(metrics)
+				
 
 		except requests.ConnectionError as error:
 			LOGGER.error('Error reporting stats: %s', error)
